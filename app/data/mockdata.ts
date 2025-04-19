6ab92fb32af8d2477d1ed46218917151d73c3e61
jM <jmkoh@hotmail.com>
2025-04-19 21:15:21 +0800

    move mockdata to /data

---
+++ app/data/mockdata.ts
@@ -0,0 +1,29 @@
+import { IconGauge, IconUser, IconCookie, IconLock, IconMessage2 } from '@tabler/icons-react';
+
+export const MOCKDATA = [
+  {
+    icon: IconGauge,
+    title: 'Tailored Financial Solutions',
+    description: 'We understand that every financial journey is unique—our services are customized to match your personal goals and needs.',
+  },
+  {
+    icon: IconUser,
+    title: 'Simple, Transparent Process',
+    description: 'No hidden fees or complex jargon. We make financial services easy to understand and totally transparent',
+  },
+  {
+    icon: IconCookie,
+    title: 'Secure & Trusted by Thousands',
+    description: 'With top-tier security and a growing community of satisfied clients, your trust is our top priority',
+  },
+  {
+    icon: IconLock,
+    title: 'Expert Support, Anytime',
+    description: 'Our dedicated team is here to guide you through every step—whether it’s mortgages, savings, or insurance',
+  },
+  {
+    icon: IconMessage2,
+    title: 'Innovative Digital Tools',
+    description: 'Manage everything from your dashboard with modern tools that keep you informed and in control—anytime, anywhere',
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -5,43 +5,12 @@ import { Carousel } from '@mantine/carousel';
 import { useMediaQuery } from '@mantine/hooks';
 import { Button, Container, Overlay, Text, Title, Box, TextInput, Textarea, Group, SimpleGrid, ThemeIcon, Paper, useMantineTheme  } from '@mantine/core';
 import classes from './HeroContentLeft.module.css';
-import { IconCookie, IconGauge, IconLock, IconMessage2, IconUser } from '@tabler/icons-react';
+//import { IconCookie, IconGauge, IconLock, IconMessage2, IconUser } from '@tabler/icons-react';
 import '@mantine/carousel/styles.css';
 
 
 
-export const MOCKDATA = [
-  {
-    icon: IconGauge,
-    title: 'Tailored Financial Solutions',
-    description:
-      'We understand that every financial journey is unique—our services are customized to match your personal goals and needs.',
-  },
-  {
-    icon: IconUser,
-    title: 'Simple, Transparent Process',
-    description:
-      'No hidden fees or complex jargon. We make financial services easy to understand and totally transparent',
-  },
-  {
-    icon: IconCookie,
-    title: 'Secure & Trusted by Thousands',
-    description:
-      'With top-tier security and a growing community of satisfied clients, your trust is our top priority',
-  },
-  {
-    icon: IconLock,
-    title: 'Expert Support, Anytime',
-    description:
-      'Our dedicated team is here to guide you through every step—whether it’s mortgages, savings, or insurance',
-  },
-  {
-    icon: IconMessage2,
-    title: 'Innovative Digital Tools',
-    description:
-      'Manage everything from your dashboard with modern tools that keep you informed and in control—anytime, anywhere',
-  },
-];
+import { MOCKDATA } from './data/mockdata'; // adjust path if needed
 
 
 interface FeatureProps {
